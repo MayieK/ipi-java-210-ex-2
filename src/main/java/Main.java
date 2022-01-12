7263648c1348d9ac4efe16395183e4736926e8b5
beal <mayiekim.beal@ipilyon.net>
2022-01-12 12:43:01 +0100

    Exercice3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
-    /* Exercice1
-    Créer les constantes suivantes dans la classe `Main` (au niveau de la classe, pas à l'intérieur de la méthode `main`).
-Utiliser des types primitifs et préfixer toutes les déclarations par `static`
-- `MAX_PTS_VIE` de type `short` et de valeur `100`
-- `PTS_BOUCLIER` de type `short` et de valeur `25`
-- `MAX_ATTAQUE_ENNEMI` de type `short` et de valeur `5`
-- `MAX_VIE_ENNEMI` de type `short` et de valeur `30`
-- `MAX_ATTAQUE_JOUEUR` de type `short` et de valeur `5`
-- `REGENARATION_BOUCLIER_PAR_TOUR` de type `short` et de valeur `10`
-     */
     public static final short MAX_PTS_VIE = 100;
     public static final short PTS_BOUCLIER = 25;
     public static final short MAX_ATTAQUE_ENNEMI = 5;
     public static final short MAX_VIE_ENNEMI = 30;
     public static final short MAX_ATTAQUE_JOUEUR = 5;
     public static final short REGENARATION_BOUCLIER_PAR_TOUR = 10;
-    /* Exercice2
-    Créer les variables suivantes dans la classe `Main` (au niveau de la classe, pas à l'intérieur de la méthode `main`).
-- `nomPersonnage` de type `String`
-- `ptsDeVie` de type `short`
-- `ptsBouclier` de type `short`
-- `nbEnnemisTues` de type `short`
-- `bouclierActif` de type `boolean` et de valeur `true`
-     */
+
     public static String nomPersonnage;
     public static short ptsDeVie;
     public static short ptsBouclier;
     public static short nbEnnemisTues;
     public static boolean bouclierActif = true;
+
+    public static void main(String[] args)
+    {
+        initPersonnage();
+    }
+        public static void initPersonnage()
+    {
+        //afficher message de saisie
+        System.out.println("Saisir le nom de votre personnage");
+                //lire la saisie utilisateur
+        Scanner scanner = new Scanner(System.in);
+        nomPersonnage = scanner.nextLine();
+                // afficher le message c'est parti
+        System.out.println("OK " + nomPersonnage + " ! C'est parti !");
+                // affecter la variable ptsDeVie
+        ptsDeVie = MAX_PTS_VIE;
+        // affecter la variable ptsBouclier
+        ptsBouclier = PTS_BOUCLIER;
+
+        scanner.close();
+    }
 }
 
