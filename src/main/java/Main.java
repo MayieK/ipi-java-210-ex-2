ef2f020958cb28c18ac82a8db61389ada08a099f
beal <mayiekim.beal@ipilyon.net>
2022-01-12 11:36:23 +0100

    Exercice1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-
-    /**
-     *
-     * @param args
+    /* Exercice1
+    Créer les constantes suivantes dans la classe `Main` (au niveau de la classe, pas à l'intérieur de la méthode `main`).
+Utiliser des types primitifs et préfixer toutes les déclarations par `static`
+- `MAX_PTS_VIE` de type `short` et de valeur `100`
+- `PTS_BOUCLIER` de type `short` et de valeur `25`
+- `MAX_ATTAQUE_ENNEMI` de type `short` et de valeur `5`
+- `MAX_VIE_ENNEMI` de type `short` et de valeur `30`
+- `MAX_ATTAQUE_JOUEUR` de type `short` et de valeur `5`
+- `REGENARATION_BOUCLIER_PAR_TOUR` de type `short` et de valeur `10`
      */
+    public static final short MAX_PTS_VIE = 100;
+    public static final short PTS_BOUCLIER = 25;
+    public static final short MAX_ATTAQUE_ENNEMI = 5;
+    public static final short MAX_VIE_ENNEMI = 30;
+    public static final short MAX_ATTAQUE_JOUEUR = 5;
+    public static final short REGENARATION_BOUCLIER_PAR_TOUR = 10;
 }
+
