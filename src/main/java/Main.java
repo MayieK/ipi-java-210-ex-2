91e41a6fef8f7cbb785fe636533633973d19d235
beal <mayiekim.beal@ipilyon.net>
2022-01-13 10:32:24 +0100

    Exercice8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static final short MAX_PTS_VIE = 100;
     public static final short PTS_BOUCLIER = 25;
     public static final short MAX_ATTAQUE_ENNEMI = 5;
@@ -13,7 +13,7 @@ public class Main {
     public static short nbEnnemisTues;
     public static boolean bouclierActif = true;
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         initPersonnage();
         affichePersonnage();
         bouclierActif = false;
@@ -22,7 +22,7 @@ public class Main {
         ennemi = attaqueJoueur(ennemi);
         System.out.println("Il reste " + ennemi + " points de vie à l'ennemi");
     }
-    public static void initPersonnage() {
+    public static void initPersonnage(){
         //afficher message de saisie
         System.out.println("Saisir le nom de votre personnage");
         //lire la saisie utilisateur
@@ -36,32 +36,39 @@ public class Main {
         ptsBouclier = bouclierActif ? PTS_BOUCLIER : 0;
         scanner.close();
     }
-    public static void affichePersonnage() {
+    public static void affichePersonnage(){
         System.out.print(Util.color(nomPersonnage, Color.GREEN) + " (" + Util.color(ptsDeVie, Color.RED));
-        if(bouclierActif) {
+        if(bouclierActif){
             System.out.print(" " + Util.color(ptsBouclier, Color.BLUE));
             System.out.print(")");
         }
     }
-    public static boolean hasard(double pourcentage) {
+    public static boolean hasard(double pourcentage){
         //pourcentage < résultat du chiffre random = true
         //sinon faux
         return pourcentage < Math.random();
     }
-    public static short nombreAuHasard(short nombre) {
+    public static short nombreAuHasard(short nombre){
         return (short) Math.round(Math.random() * nombre);
     }
-    public static short attaqueJoueur(short ptsVieEnnemi) {
+    public static short attaqueJoueur(short ptsVieEnnemi){
         //Déterminer la force de l'attaque du joueur
-        short forceAttague = nombreAuHasard(MAX_ATTAQUE_JOUEUR);
+        short forceAttaque = nombreAuHasard(MAX_ATTAQUE_JOUEUR);
         //Retrancher les points de l'attaque de l'ennemi sur les points de vie de l'ennemi
-        ptsVieEnnemi -= forceAttague;
+        ptsVieEnnemi -= forceAttaque;
         //Afficher les caractéristiques de l'attaque
         System.out.println(Util.color(nomPersonnage, Color.GREEN)
                 + " attaque l'" + Util.color("ennemi", Color.YELLOW) + " ! Il lui fait perdre "
-                + Util.color(forceAttague, Color.PURPLE) + " points de dommages");
+                + Util.color(forceAttaque, Color.PURPLE) + " points de dommages");
         //Retourner le nombre de points de vie de l'ennemi après l'attaque
         return ptsVieEnnemi;
+    }
+    public static void attaqueEnnemi(){
+        short forceDommage = nombreAuHasard(MAX_ATTAQUE_ENNEMI);
+        if(ptsBouclier >= forceDommage){
+            ptsBouclier -= forceDommage;
+            else(ptsDeVie -= forceDommage);
+        }
     }
 
 }
